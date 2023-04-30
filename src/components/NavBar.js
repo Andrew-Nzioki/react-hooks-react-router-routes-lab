297622347fab7df42a9e6aae1c69a700200107fd
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-30 21:49:07 +0300

    react dom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <NavLink>
+    
+  </NavLink>
+  </div>;
 }
 
 export default NavBar;
